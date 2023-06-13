ec9fddd868bdc294ce8b0ee1fb575f937fe494ec
Евгений Левдик <eugene.levdik@gmail.com>
2023-06-13 16:10:36 +0300

    Separate plotting from data analysis

--- example.py
+++ example.py
@@ -1,12 +1,39 @@
+import matplotlib.pyplot as plt
+
 from frap import process_frap_images, process_diffusion
+from frap import gaussian_distribution
 
 
 if __name__ == '__main__':
-    w = process_frap_images(directory_path='test_data',
-                            scan_size_um=425,
-                            show_baseline=True,
-                            show_curves=3)
-    D, diameter = process_diffusion(w,
-                                    t=0.392,
-                                    show_plot=True)
-    print(f'Hydrodynamic diameter: {round(diameter * 1e9, 3)} nm')
+    time_between_frames = 0.392
+    scan_size_um = 425
+
+    # approximate frap images with gaussian distribution and get w(t)
+    w, frap_raw_data = process_frap_images(directory_path='test_data', scan_size_um=425)
+
+    # linearize w²(t) and get hydrodynamic diameter
+    D, diameter, linearization_data = process_diffusion(w, t=time_between_frames)
+    print(f'Hydrodynamic diameter: {round(diameter, 3)} nm')
+
+    # visualization
+    # intensity and gaussian approximation
+    n_frames_to_plot = 5
+    for i in range(n_frames_to_plot):
+        x, experimental_data, gaussian_params = frap_raw_data[i]
+        plot_color = next(plt.gca()._get_lines.prop_cycler)['color']
+        plt.plot(x, experimental_data, linewidth=0.2, color=plot_color, label='_nolegend_')
+        plt.plot(x, gaussian_distribution(x, *gaussian_params), color=plot_color)
+    plt.title('Bleached line intensity approximated with Gaussian')
+    plt.xlabel('x, μm')
+    plt.ylabel('Intensity')
+    plt.legend([f'frame {i + 1}' for i in range(n_frames_to_plot)])
+    plt.show()
+
+    # diffusion linearization
+    t, w2, linearization_params = linearization_data
+    k, b = linearization_params
+    plt.plot(t, w2, marker='o', linewidth=0)
+    plt.plot(t, t * k + b)
+    plt.xlabel('Time, s')
+    plt.ylabel('w², μm²')
+    plt.show()
--- frap.py
+++ frap.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 from collections.abc import Iterable
@@ -17,7 +16,11 @@ __license__ = "Public Domain"
 __version__ = "1.0"
 
 
-def process_diffusion(w, t, linear_points=None, remove_outliers=False, show_plot=False,
+def gaussian_distribution(x, y0, a, x0, w):
+    return y0 + a * np.exp(-2 * ((x - x0) ** 2) / (w ** 2))
+
+
+def process_diffusion(w, t, linear_points=None, remove_outliers=False,
                       room_temperature_celsius=23., solvant_viscosity=0.9358e-3):
     """Calculates diffusion coefficient and hydrodynamic diameter of particles based on FRAP data.
 
@@ -33,8 +36,6 @@ def process_diffusion(w, t, linear_points=None, remove_outliers=False, show_plot
         List of indices of outliers to be removed from `w`
         If False, no points are removed (False by default)
         If True, outliers are removed automatically (not implemented yet)
-    show_plot : bool, optional
-        If True, shows plot of :math:`w^2(t)`, False by default
     room_temperature_celsius : float, optional
         Room temperature in Celsius, 23 by default
     solvant_viscosity : float, optional
@@ -44,12 +45,12 @@ def process_diffusion(w, t, linear_points=None, remove_outliers=False, show_plot
     ----------
     float : diffusion coefficient, m^2 / s
     float : hydrodynamic radius, nm
+    tuple : linearization data (t, w2, (k, b))
     """
 
     if isinstance(t, float):
         t = np.arange(len(w)) * t
 
-    w2 = np.array(w) ** 2
     if isinstance(remove_outliers, Iterable):
         w = np.delete(w, remove_outliers)
         t = np.delete(t, remove_outliers)
@@ -57,27 +58,23 @@ def process_diffusion(w, t, linear_points=None, remove_outliers=False, show_plot
         # TODO: remove outliers automatically
         pass
 
+    w2 = np.array(w) ** 2
+
     if linear_points is None:
         linear_points = len(w)
 
-    k, b = np.polyfit(t[:linear_points - 1], w2[:linear_points - 1], 1)
+    linearization_params = np.polyfit(t[:linear_points - 1], w2[:linear_points - 1], 1)
+    k, b = linearization_params
 
-    if show_plot:
-        plt.plot(t, w2, marker='o', linewidth=0)
-        plt.plot(t, t * k + b)
-        plt.xlabel('Time, s')
-        plt.ylabel('w², μm²')
-        plt.grid()
-        plt.show()
-
-    diffusion_coeff = k * 1e-12 / 8  # m^2 / s
+    diffusion_coeff_m2s = k * 1e-12 / 8
     room_temperature_kelvin = room_temperature_celsius + zero_Celsius
-    diameter = k_b * room_temperature_kelvin / (3 * np.pi * solvant_viscosity * diffusion_coeff)
-    return diffusion_coeff, diameter
+    diameter_nm = 1e9 * k_b * room_temperature_kelvin / (3 * np.pi * solvant_viscosity * diffusion_coeff_m2s)
+    linearization_data = (t, w2, linearization_params)
+    return diffusion_coeff_m2s, diameter_nm, linearization_data
 
 
 def process_frap_images(directory_path, scan_size_um,
-                        show_curves=0, show_baseline=False,
+                        baseline_correction='linear',
                         frames_without_bleaching=2, crop=(0.3, 0.03)):
     """
     Processes microscopic images of diffusion kinetics of a bleached horizontal line (FRAP)
@@ -88,18 +85,17 @@ def process_frap_images(directory_path, scan_size_um,
         Path to directory with microscopic images
     scan_size_um : float
         Scanning area size, μm
-    show_curves : int, optional
-        Number of first intensity distributions to show, 0 by default
-    show_baseline : bool, optional
-        If True, plotted baseline is shown, False by default
+    baseline_correction : str, optional
+        Type of baseline correction. 'linear' (by default) and None are possible
     frames_without_bleaching : int, optional
         Number of images taken before bleaching, 2 by default
     crop : iterable, optional
-        Relative size edges to crop, by default 0.3 vertically, 0.03 horizontally
+        Relative size of edges to crop, by default 0.3 vertically, 0.03 horizontally
 
     Returns
     ----------
-    list : list of full width at half minimum intensity of bleached line
+    list : list of full width at half minimum intensity of bleached line in micrometers
+    list : list of tuples (x_um, experimental_data, gaussian_parameters) for each frame
     """
 
     image_filenames = os.listdir(directory_path)
@@ -124,40 +120,29 @@ def process_frap_images(directory_path, scan_size_um,
 
     x = np.linspace(0, len(initial_data) * scan_size_um / image_size_px, len(initial_data))
 
-    k, b = np.polyfit(x, initial_data, 1)
-    baseline = k * x + b
-
-    if show_baseline:
-        plt.plot(x, initial_data)
-        plt.plot(x, baseline)
-        plt.xlabel('x, μm')
-        plt.ylabel('Intensity')
-        plt.show()
+    if baseline_correction is not None:
+        baseline_correction = baseline_correction.lower()
+        if baseline_correction == 'linear':
+            k, b = np.polyfit(x, initial_data, 1)
+            baseline = k * x + b
+        else:
+            raise ValueError(f'No such type of baseline correction: {baseline_correction}')
 
     w = []
+    all_images_data = []
     for i in range(number_of_images):
         with Image.open(image_filename_prefix + filename_postfix(i + frames_without_bleaching)) as image:
             image = image.convert('L')
             data = np.array(image).astype(float) / 255
         data = data[crop_x:-crop_x, crop_y:-crop_y]
         data = np.mean(data, 1)
-        data /= baseline
+        if baseline_correction is not None:
+            data /= baseline
 
-        def gaussian_distribution(x, y0, a, x0, w):
-            return y0 + a * np.exp(-2 * ((x - x0) ** 2) / (w ** 2))
         guess_params = data[0], min(data) - data[0], x[np.argmin(data)], 0.2 * image_size_px
         params, cov = curve_fit(gaussian_distribution, x, data, p0=guess_params)
-        standard_deviation = np.sqrt(np.diag(cov))
-        # TODO: reject outliers with high standard_deviation
         w.append(params[-1])
 
-        if i < show_curves:
-            plot_color = next(plt.gca()._get_lines.prop_cycler)['color']
-            plt.plot(x, data, linewidth=0.2, color=plot_color, label='_nolegend_')
-            plt.plot(x, gaussian_distribution(x, *params), color=plot_color)
+        all_images_data.append((x, data, params))
 
-    if show_curves > 0:
-        plt.xlabel('x, μm')
-        plt.ylabel('Intensity')
-        plt.show()
-    return w
+    return w, all_images_data
